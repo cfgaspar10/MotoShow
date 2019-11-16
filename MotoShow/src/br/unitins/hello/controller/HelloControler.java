f018bb8bad55e4264d822bce4a21be4f69a42dbc
carlesandrogaspar <carlesandrogaspar@localhost>
2019-11-16 01:48:20 -0300

    Inicial

---
+++ MotoShow/src/br/unitins/hello/controller/HelloControler.java
@@ -0,0 +1,15 @@
+package br.unitins.hello.controller;
+
+import java.io.Serializable;
+
+import javax.inject.Named;
+
+@Named
+public class HelloControler implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3885496959302936834L;
+
+}
